9d26f0e78b4b30f73cd23721a113f49891ed7d04
Henry25t <henrycarias96@gmail.com>
2025-07-26 13:54:06 -0600

    first commit

---
+++ javascript/menu.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+  const cv = document.querySelector(".cv");
+
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("open");
+    menu.classList.toggle("open");
+  });
+
+  cv.addEventListener("click", () => {
+    
+  })
+});
